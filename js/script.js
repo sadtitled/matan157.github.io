c081581acb04ccdef5919114d2f03a6f7fd93538
Matan Uchen <matan.uchen@gmail.com>
2015-02-02 11:12:42 -0500

    Base Files

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$(document).ready(function() {
+	$('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>&copy; Z&J Management, LLC.</small>';
+			}
+		}
+	});
+});
+
+ $(function(){
+      $(".typing").typed({
+        strings: ["developer.", "designer.", "musician.", "problem solver."],
+        typeSpeed: 0
+      });
+  });
